f911f502d847a5846233fe510db1821210062be1
Louis Lainé <louis.laine7@gmail.com>
2015-12-25 20:01:53 +0100

    Eslint add

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,10 @@
 
 import request from 'request';
 
+const param = '?cli=1';
 const API_ENDPOINT = 'http://cmdto.com/api/v1/apps/fm/genres/';
+
+const endpointWithParams = (style) => API_ENDPOINT + style + param;
 
 export default class Api {
   constructor(...params) {
@@ -23,7 +26,9 @@ export default class Api {
 
   getSongFromGenre(genreName) {
     return new Promise(function(resolve, reject) {
-      request(API_ENDPOINT + genreName, (error, response, body) => {
+      let URL = endpointWithParams(genreName);
+      console.info(`Fetching playlist with endpoint ${URL}`);
+      request(URL, (error, response, body) => {
         if(!error && response.statusCode === 200) {
           resolve(JSON.parse(body));
         } else {
@@ -34,11 +39,10 @@ export default class Api {
   }
 
   getStreamUrl(streamUrl) {
-
     const opts = {
       url: streamUrl + '?client_id=26fb3c513c8e0e2c18a75e6174f4ca70',
       followAllredirects:true
-    }
+    };
 
     return new Promise(function(resolve, reject) {
       request(opts,
---
+++ src/cmdfm.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+const jukeboxReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'DISPLAY_STYLES':
+      break;
+    case 'PLAY':
+
+      break;
+    default:
+      return state;
+  }
+};
